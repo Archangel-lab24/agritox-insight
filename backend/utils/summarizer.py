2d100a5af831d096f88fd81ae1a678861271aca8
Archangel-lab24 <byronbaron@hotmail.com>
2025-12-13 14:46:56 +0100

    Delete backend/utils directory

--- backend/utils/summarizer.py
+++
@@ -1,9 +0,0 @@
-
-def summarize(pubchem, epa):
-    risk = "Unknown"
-    if pubchem:
-        risk = "Low to moderate mammalian toxicity"
-    return {
-        "environmental_risk": risk,
-        "note": "Rule-based summary (no ML)"
-    }
